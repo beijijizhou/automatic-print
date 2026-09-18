69ed98a75e2cdd73bdd8d4aed3221b396e704fe3
Hongzhong Hu <90345577+beijijizhou@users.noreply.github.com>
2026-09-18 12:53:20 -0400

    fix: detect existing Python 3.12 in bootstrap

--- automatic_print/__init__.py
+++ automatic_print/__init__.py
@@ -2,7 +2,7 @@
 
 from .updates.versioning import release_display
 
-__version__ = "0.1.285"
+__version__ = "0.1.286"
 __release_date__ = "2026-09-18"
-__release_iteration__ = 25
+__release_iteration__ = 26
 __version_display__ = release_display(__release_date__, __release_iteration__)
